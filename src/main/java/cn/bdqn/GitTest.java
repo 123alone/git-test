dd46d2f01bf43b8407906e5d73b58d72106f33ad
123alone <74649172+123alone@users.noreply.github.com>
2021-06-25 21:15:53 +0800

    pull commit

--- src/main/java/cn/bdqn/GitTest.java
+++ src/main/java/cn/bdqn/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("撒娇大手大脚啊");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
