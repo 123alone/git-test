dc18feaa5f5f2f6715476343d0d739ffe8fb59f1
cdh <1564497764@qq.com>
2021-06-25 20:37:48 +0800

    master commit 8

--- src/main/java/cn/bdqn/GitTest.java
+++ src/main/java/cn/bdqn/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git4");
         System.out.println("hello git6");
+        System.out.println("撒娇大手大脚啊");
     }
 }
