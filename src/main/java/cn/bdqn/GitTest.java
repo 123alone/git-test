cfc54caa21c4b41230b16afacffdd59223ea0b41
cdh <1564497764@qq.com>
2021-06-25 21:01:19 +0800

    push commit 8

--- src/main/java/cn/bdqn/GitTest.java
+++ src/main/java/cn/bdqn/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git6");
         System.out.println("撒娇大手大脚啊");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
